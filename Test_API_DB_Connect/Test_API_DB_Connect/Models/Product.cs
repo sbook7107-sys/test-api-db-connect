88f509140cc19c0a6e1012b31c60a4782e2be12c
PhucHoang200 <phucphuc2004444@gmail.com>
2025-11-11 00:47:55 +0700

    Add setup project

---
+++ Test_API_DB_Connect/Test_API_DB_Connect/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_API_DB_Connect.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public decimal? Price { get; set; }
+}
