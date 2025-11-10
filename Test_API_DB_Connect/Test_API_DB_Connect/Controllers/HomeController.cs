1fe0eb73ffbe8f45df3e52f7116258b1649773a9
PhucHoang200 <phucphuc2004444@gmail.com>
2025-11-11 01:53:32 +0700

    Add homecontroller.cs file
    Co-authored-by: sbook7107-sys <sbook7107@gmail.com>

---
+++ Test_API_DB_Connect/Test_API_DB_Connect/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test_API_DB_Connect.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
